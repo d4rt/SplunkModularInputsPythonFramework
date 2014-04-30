1b9f1e96af133f09d422137b5a37c2b9c8aa272b
Duncan Turnbull <dunc@nturnbull.com>
2014-04-30 11:28:50 +0100

    wip 4

--- implementations/tshark/bin/tshark.py
+++ implementations/tshark/bin/tshark.py
@@ -133,4 +133,90 @@ def do_run():
         logging.error("Looks like an error replacing environment variables: %s" % str(e))  
 
 
-  
+ #read XML configuration passed from splunkd, need to refactor to support single instance mode
+def get_input_config():
+    config = {}
+
+    try:
+        # read everything from stdin
+        config_str = sys.stdin.read()
+
+        # parse the config XML
+        doc = xml.dom.minidom.parseString(config_str)
+        root = doc.documentElement
+        conf_node = root.getElementsByTagName("configuration")[0]
+        if conf_node:
+            logging.debug("XML: found configuration")
+            stanza = conf_node.getElementsByTagName("stanza")[0]
+            if stanza:
+                stanza_name = stanza.getAttribute("name")
+                if stanza_name:
+                    logging.debug("XML: found stanza " + stanza_name)
+                    config["name"] = stanza_name
+
+                    params = stanza.getElementsByTagName("param")
+                    for param in params:
+                        param_name = param.getAttribute("name")
+                        logging.debug("XML: found param '%s'" % param_name)
+                        if param_name and param.firstChild and \
+                           param.firstChild.nodeType == param.firstChild.TEXT_NODE:
+                            data = param.firstChild.data
+                            config[param_name] = data
+                            logging.debug("XML: '%s' -> '%s'" % (param_name, data))
+
+        checkpnt_node = root.getElementsByTagName("checkpoint_dir")[0]
+        if checkpnt_node and checkpnt_node.firstChild and \
+           checkpnt_node.firstChild.nodeType == checkpnt_node.firstChild.TEXT_NODE:
+            config["checkpoint_dir"] = checkpnt_node.firstChild.data
+
+        if not config:
+            raise Exception, "Invalid configuration received from Splunk."
+
+        
+    except Exception, e:
+        raise Exception, "Error getting Splunk configuration via STDIN: %s" % str(e)
+
+    return config
+
+#read XML configuration passed from splunkd, need to refactor to support single instance mode
+def get_validation_config():
+    val_data = {}
+
+    # read everything from stdin
+    val_str = sys.stdin.read()
+
+    # parse the validation XML
+    doc = xml.dom.minidom.parseString(val_str)
+    root = doc.documentElement
+
+    logging.debug("XML: found items")
+    item_node = root.getElementsByTagName("item")[0]
+    if item_node:
+        logging.debug("XML: found item")
+
+        name = item_node.getAttribute("name")
+        val_data["stanza"] = name
+
+        params_node = item_node.getElementsByTagName("param")
+        for param in params_node:
+            name = param.getAttribute("name")
+            logging.debug("Found param %s" % name)
+            if name and param.firstChild and \
+               param.firstChild.nodeType == param.firstChild.TEXT_NODE:
+                val_data[name] = param.firstChild.data
+
+    return val_data
+
+if __name__ == '__main__':
+      
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--scheme":           
+            do_scheme()
+        elif sys.argv[1] == "--validate-arguments":
+            do_validate()
+        else:
+            usage()
+    else:
+        do_run()
+        
+    sys.exit(0) 
